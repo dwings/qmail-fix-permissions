0e6ba36f055d0f62aa64ab5b0a151bbe4cedab20
dwings <dwing@weingarten-net.de>
2014-01-22 19:05:25 +0100

    Update qmail_fixpermissions.sh
    Added check, if path to qmail is valid!

--- qmail_fixpermissions.sh
+++ qmail_fixpermissions.sh
@@ -4,21 +4,31 @@ if [ "$(whoami)" != "root" ];
    echo "Needs to be run as root, [x]"
    exit 1
 else
-
-echo "This little script will fix your qmail permissions (if broken)!"
-echo "Please enter the path to qmail (default: /var/qmail)"
-read dir
-dir=${dir:=/var/qmail}
-
-echo "using $dir..."
-echo "Please enter the username for your maildaemon (default: popuser)"
-read xuser
-xuser=${xuser:=popuser}
-echo "using $xuser..."
-echo "fixing users"
-chown $xuser:$xuser $dir/mailnames -R $*
-echo "fixing permissions"
-chmod 0775 $dir/mailnames -R $*
-echo "done"
-exit 0
+	echo "This little script will fix your qmail permissions (if broken)!"
+	echo -n "Please enter the path to qmail (default: /var/qmail): "; 
+	read dir
+	dir=${dir:=/var/qmail}
+	echo "Using: $dir"
+	
+	echo -n "checking path for qmail installation..."
+	sleep 1s
+	if [ ! -f $dir/bin/qmail-start ];
+	then
+	   echo "Could not find qmail installation in $dir"
+	   exit 0
+	fi
+	echo "success"
+	echo -n "Please enter the username for your maildaemon (default: popuser): "; read xuser
+	xuser=${xuser:=popuser}
+	echo "Using: $xuser"
+	
+	echo -n "fixing users..."
+	sleep 2s
+	chown $xuser:$xuser $dir/mailnames -R $*
+	echo "done"
+	echo -n "fixing permissions..."
+	sleep 2s
+	chmod 0775 $dir/mailnames -R $*
+	echo "done"
+	exit 0
 fi
